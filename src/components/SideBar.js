2d6a16c7167acbae673b17027ef4d12ca2b9456f
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-08-21 14:56:23 +0700

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,7 +8,7 @@ function SideBar() {
         <a href="https://calendar.google.com/calendar">
           <img
             className="h-6 w-6"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Calendar_icon_%282020%29.svg/1024px-Google_Calendar_icon_%282020%29.svg.png"
+            src="https://www.hireauthority.com/wp-content/uploads/2016/12/QOH-Step-1-image.png"
             alt="calender-logo"
           />
         </a>
@@ -17,7 +17,7 @@ function SideBar() {
         <a href="https://keep.google.com">
           <img
             className="h-6 w-6"
-            src="https://cdn-icons-png.flaticon.com/512/2965/2965358.png"
+            src="https://aztecexplorers.com/wp-content/uploads/2019/03/qoh-step-2-image.png"
             alt="keep-logo"
           />
         </a>
@@ -26,29 +26,13 @@ function SideBar() {
         <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.tasks&hl=en&gl=US">
           <img
             className="h-6 w-6"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Google_Tasks_2021.svg/1200px-Google_Tasks_2021.svg.png"
+            src="https://aztecexplorers.com/wp-content/uploads/2019/03/qoh-step-3-image.png"
             alt="tasks-logo"
           />
         </a>
       </div>
-      <div className="mb-11">
-        <a href="https://contacts.google.com/">
-          <img
-            className="h-6 w-6"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Google_Contacts_icon.svg/1200px-Google_Contacts_icon.svg.png"
-            alt="contacts-logo"
-          />
-        </a>
-      </div>
-      <div className="mb-11">
-        <a href="https://www.google.com/maps">
-          <img
-            className="h-6 w-6"
-            src="https://cdn-icons-png.flaticon.com/512/355/355980.png"
-            alt="maps-logo"
-          />
-        </a>
-      </div>
+
+
       <div className="mb-11">
         <AddOutlinedIcon />
       </div>
